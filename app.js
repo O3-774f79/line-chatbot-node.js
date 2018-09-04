3a1fa170cc413b3d0cb6a796d7cb50a0b9e1165c
ananchai <ananchai.pa@gmail.com>
2018-09-04 13:51:18 +0700

    add new code

--- app.js
+++ app.js
@@ -28,8 +28,54 @@ function sendText(sender, text) {
     let data = {
         to: sender,
         messages: [{
-            type: 'text',
-            text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก'
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [{
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=111"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=111"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/111"
+                            }
+                        ]
+                    },
+                    {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }
+                ]
+            }
         }]
 
     }
