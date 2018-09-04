c97965643e8b79bc752eef186014e6ca027e7bf9
ananchai <ananchai.pa@gmail.com>
2018-09-04 15:32:59 +0700

    edit flex message

--- app.js
+++ app.js
@@ -37,34 +37,38 @@ const handleDetailAfterSender = (sender, text) => {
             "altText": "This is a Flex Message",
             "contents": {
                 "type": "bubble",
-                "header": {
-                    "type": "text",
-                    "text": "เอกสาร"
-                },
                 "body": {
-                    "type": "box",
-                    "layout": "horizontal",
-                    "contents": [{
+                    "type": "bubble",
+                    "header": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [{
                             "type": "text",
-                            "text": "Hello",
-                            "color": "#00ff00",
-                            "flex": 0
-                        },
-                        {
+                            "text": "header"
+                        }]
+                    },
+                    "hero": {
+                        "type": "image",
+                        "url": "https://example.com/flex/images/image.jpg",
+                        "size": "full",
+                        "aspectRatio": "2:1"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [{
                             "type": "text",
-                            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                            "wrap": true,
-                            "color": "#ff0000",
-                            "flex": 2
-                        },
-                        {
+                            "text": "body"
+                        }]
+                    },
+                    "footer": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [{
                             "type": "text",
-                            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                            "wrap": true,
-                            "color": "#0000ff",
-                            "flex": 3
-                        }
-                    ]
+                            "text": "footer"
+                        }]
+                    }
                 }
             }
 
