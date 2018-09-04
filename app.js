970c887319155d458066d53eb49c777b90654bf7
ananchai <ananchai.pa@gmail.com>
2018-09-04 14:06:06 +0700

    add new code

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post('/webhook', (req, res) => {
     if (text === 'ca' || text === 'CA' || text == 'Ca') {
         sendText(sender, text)
     }
-    res.sendSta tus(200)
+    res.sendStatus(200)
 })
 
 function sendText(sender, text) {
@@ -73,8 +73,107 @@ function sendText(sender, text) {
                                 "uri": "http://example.com/page/222"
                             }
                         ]
-                    },
-                    {
+                    }, {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }, {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }, {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }, {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }, {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [{
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }, {
                         "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
                         "title": "this is menu",
                         "text": "description",
