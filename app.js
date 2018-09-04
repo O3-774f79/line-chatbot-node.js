551d86e5523093edd3fe51f584601c87ae418012
ananchai <ananchai.pa@gmail.com>
2018-09-04 12:11:02 +0700

    add message type

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
@@ -15,29 +17,58 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token,msg,type) {
+
+function reply(reply_token, msg, type) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {NZmbMY1LEsmLHz2dr76gAuyIk/0XmrcO+QaRJP5IwG6DkBsdCZ85lSxahZTsXox5ArGCkOCre9U3BPy56aYZH6NBS3nMX0uEfBbJR4sxz1vj3S1mE/u/oy7rctpXo94vCMHlRQZCQB4CxEetsHrHTgdB04t89/1O/w1cDnyilFU=}'
     }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
+    const body
+    if (msg === 'hello') {
+        body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                    type: 'text',
+                    text: 'Hello'
+                },
+                {
+                    type: 'text',
+                    text: 'How are you?'
+                }
+            ]
+        })
+    } else if (msg === 'ca') {
+        body = JSON.stringify({
+            reply_token: reply_token,
+            messages = {
+                "type": "template",
+                "altText": "this is a confirm template",
+                "template": {
+                    "type": "confirm",
+                    "text": "Are you sure?",
+                    "actions": [{
+                        "type": "message",
+                        "label": "Yes",
+                        "text": "yes"
+                    }, {
+                        "type": "message",
+                        "label": "No",
+                        "text": "no"
+                    }]
+                }
+            }
+        })
+    }
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
         body: body
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode);
+        console.log("error = " + err)
+        console.log("body = "+ body)
     });
     console.log(msg)
     console.log(type)
+    console.log(body)
 }
