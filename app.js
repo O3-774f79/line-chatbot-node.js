4ca7b5b93a91f3e3cd289df4dd8edc47d8332095
ananchai <ananchai.pa@gmail.com>
2018-09-04 15:23:06 +0700

    edit flex message

--- app.js
+++ app.js
@@ -33,26 +33,37 @@ const handleDetailAfterSender = (sender, text) => {
     let data = {
         to: sender,
         messages: [{
-            "type": "bubble",
-            "body": {
-                "type": "box",
-                "layout": "horizontal",
-                "contents": [{
-                        "type": "text",
-                        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                        "wrap": true,
-                        "color": "#ff0000",
-                        "flex": 2
-                    },
-                    {
-                        "type": "text",
-                        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                        "wrap": true,
-                        "color": "#0000ff",
-                        "flex": 3
-                    }
-                ]
+            "type": "flex",
+            "altText": "This is a Flex Message",
+            "contents": {
+                "type": "bubble",
+                "body": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [{
+                            "type": "text",
+                            "text": "Hello",
+                            "color": "#00ff00",
+                            "flex": 0
+                        },
+                        {
+                            "type": "text",
+                            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                            "wrap": true,
+                            "color": "#ff0000",
+                            "flex": 2
+                        },
+                        {
+                            "type": "text",
+                            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                            "wrap": true,
+                            "color": "#0000ff",
+                            "flex": 3
+                        }
+                    ]
+                }
             }
+
         }]
     }
     request({
