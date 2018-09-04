f755ba0c9eac261d40df5b86629cc2ffa702fb41
ananchai <ananchai.pa@gmail.com>
2018-09-04 11:54:25 +0700

    add var msg

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+function reply(reply_token,msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {NZmbMY1LEsmLHz2dr76gAuyIk/0XmrcO+QaRJP5IwG6DkBsdCZ85lSxahZTsXox5ArGCkOCre9U3BPy56aYZH6NBS3nMX0uEfBbJR4sxz1vj3S1mE/u/oy7rctpXo94vCMHlRQZCQB4CxEetsHrHTgdB04t89/1O/w1cDnyilFU=}'
@@ -36,4 +37,5 @@ function reply(reply_token) {
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode);
     });
+    console.log(msg)
 }
