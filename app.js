a35eca18cb771e1133fb545a3f9430bd61c856f9
ananchai <ananchai.pa@gmail.com>
2018-09-04 15:36:59 +0700

    edit flex message

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ const handleDetailAfterSender = (sender, text) => {
                             "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                             // "wrap": true,
                             "color": "#ff0000",
-                            "flex": 2
                         },
                         {
                             "type": "text",
