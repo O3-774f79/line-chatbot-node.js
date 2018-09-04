93180adaa2666424b53617e1113c88af132401b8
ananchai <ananchai.pa@gmail.com>
2018-09-04 15:26:42 +0700

    edit flex message

--- app.js
+++ app.js
@@ -40,24 +40,28 @@ const handleDetailAfterSender = (sender, text) => {
                 "body": {
                     "type": "box",
                     "layout": "horizontal",
-                    "contents": [
-                        {
+                    "contents": [{
                             "type": "text",
                             "text": "เอกสาร"
-                        },            
-                        {
+                        }, {
                             "type": "text",
-                            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                            "wrap": true,
-                            "color": "#ff0000",
+                            "text": "Hello",
+                            "color": "#00ff00",
                             "flex": 0
                         },
                         {
                             "type": "text",
                             "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                             "wrap": true,
-                            "color": "#0000ff",
+                            "color": "#ff0000",
                             "flex": 2
+                        },
+                        {
+                            "type": "text",
+                            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                            "wrap": true,
+                            "color": "#0000ff",
+                            "flex": 3
                         }
                     ]
                 }
