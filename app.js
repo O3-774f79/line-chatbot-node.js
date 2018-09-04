65ca86209790e36fb366802ebbbe80d7a631d517
ananchai <ananchai.pa@gmail.com>
2018-09-04 12:18:36 +0700

    edit parameter

--- app.js
+++ app.js
@@ -23,18 +23,14 @@ function reply(reply_token, msg, type) {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {NZmbMY1LEsmLHz2dr76gAuyIk/0XmrcO+QaRJP5IwG6DkBsdCZ85lSxahZTsXox5ArGCkOCre9U3BPy56aYZH6NBS3nMX0uEfBbJR4sxz1vj3S1mE/u/oy7rctpXo94vCMHlRQZCQB4CxEetsHrHTgdB04t89/1O/w1cDnyilFU=}'
     }
+    const message
     if (msg === 'hello') {
-        let message = JSON.stringify({
+        message = JSON.stringify({
             replyToken: reply_token,
-            messages: [{
-                    type: 'text',
-                    text: 'Hello'
-                },
-                {
-                    type: 'text',
-                    text: 'How are you?'
-                }
-            ]
+            messages: {
+                type: 'text',
+                text: 'Hello'
+            },
         })
     }
     console.log(message)
